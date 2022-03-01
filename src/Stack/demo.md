677c5a9ba1b142d20765af4c73cd7bf8ade67999
kyriejoshua <withoutIverson@outlook.com>
2022-03-01 22:39:47 +0800

    :wrench: update Stack

--- src/Stack/demo.md
+++ src/Stack/demo.md
@@ -3,7 +3,7 @@
 ## Usage
 
 ```typescript
-import Stack from './Stack;
+import Stack, { MyStack } from './Stack;
 
 const stack = new Stack(5);
 console.log(stack); // []
@@ -19,4 +19,21 @@ console.log(stack); // [3, 9, 7]
 console.log(stack.peek()); // 7
 console.log(stack.removeFirst()); // 7
 console.log(stack.peekFirst()); // 9
+
+const myStack: MyStack<number> = new MyStack();
+console.log(myStack.empty()); // true
+myStack.push(1);
+console.log(myStack); // MyStack { outQueue: [ 1 ], inQueue: [] }
+myStack.push(3);
+console.log(myStack); // MyStack { outQueue: [ 3, 1 ], inQueue: [] }
+myStack.push(5);
+myStack.push(7);
+myStack.push(11);
+console.log(myStack); // MyStack { outQueue: [ 11, 7, 5, 3, 1 ], inQueue: [] }
+console.log(myStack.pop()); // 11
+console.log(myStack); // MyStack { outQueue: [ 7, 5, 3, 1 ], inQueue: [] }
+console.log(myStack.top()); // 7
+console.log(myStack.pop()); // 7
+console.log(myStack.pop()); // 5
+console.log(myStack.empty()); // false
 ```
--- src/Stack/index.ts
+++ src/Stack/index.ts
@@ -75,3 +75,66 @@ export default class Stack<T> {
     return this.peek();
   }
 }
+
+/**
+ * @description: 两个队列实现栈
+ * @param {T}
+ * @return {MyStack}
+ */
+export class MyStack<T> {
+  private outQueue: T[]; // 用于保存栈的弹出顺序
+  private inQueue: T[];
+
+  constructor() {
+    this.outQueue = [];
+    this.inQueue = [];
+  }
+
+  /**
+   * @description: 通过两次转换使得队列顺序变成栈的顺序
+   * [3, 1], [] => [3, 1], [5] => [], [5, 3, 1] => [5, 3, 1], []
+   * @return {void}
+   */
+  in2out(): void {
+    while (this.outQueue.length) {
+      this.inQueue.push(this.outQueue.shift());
+    }
+    while (this.inQueue.length) {
+      this.outQueue.push(this.inQueue.shift());
+    }
+  }
+
+  /**
+   * @description: 往栈中推入数据
+   * @param {T} value
+   * @return {void}
+   */
+  push(value: T): void {
+    this.inQueue.push(value);
+    this.in2out();
+  }
+
+  /**
+   * @description: 栈中弹出数据
+   * @return {T}
+   */
+  pop(): T {
+    return this.outQueue.shift();
+  }
+
+  /**
+   * @description: 取栈尾的数据
+   * @return {T}
+   */
+  top(): T {
+    return this.outQueue[0];
+  }
+
+  /**
+   * @description: 栈是否为空
+   * @return {boolean}
+   */
+  empty(): boolean {
+    return !this.outQueue.length;
+  }
+}
